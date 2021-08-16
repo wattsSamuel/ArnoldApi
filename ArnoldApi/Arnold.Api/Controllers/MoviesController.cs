4f458af634e5b94fbb91e32ff4747255b077d676
wattsSamuel <srlwatts8@gmail.com>
2021-08-16 22:26:39 +0100

    initial commit

---
+++ ArnoldApi/Arnold.Api/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arnold.Api.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            return Ok(MoviesDataSource.Current.Movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            var movieToReturn = MoviesDataSource.Current.Movies.FirstOrDefault(i => i.Id == id);
+
+            if (movieToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movieToReturn);
+        }
+    }
+}
---
+++ ArnoldApi/Arnold.Api/MoviesDataSource.cs
@@ -0,0 +1,30 @@
+﻿using Arnold.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arnold.Api
+{
+    public class MoviesDataSource
+    {
+        public static MoviesDataSource Current { get; } = new MoviesDataSource();
+
+        public List<MoviesDto> Movies { get; set; }
+
+        public MoviesDataSource()
+        {
+            Movies = new List<MoviesDto>()
+            {
+                new MoviesDto()
+                {
+                    Id = 1,
+                    Name = "Commando",
+                    Description = "A retired Special Forces colonel tries to save his daughter," +
+                    " who was abducted by his former subordinate. A retired Special Forces colonel" +
+                    " tries to save his daughter, who was abducted by his former subordinate."
+                }
+            };
+        }
+    }
+}
