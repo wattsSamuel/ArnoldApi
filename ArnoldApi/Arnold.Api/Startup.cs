85bc6eddb2ecadf62dd2b69d60a9c54be6926989
wattsSamuel <srlwatts8@gmail.com>
2021-08-17 21:52:44 +0100

    new

--- ArnoldApi/Arnold.Api/Startup.cs
+++ ArnoldApi/Arnold.Api/Startup.cs
@@ -23,6 +23,7 @@ namespace Arnold.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +37,16 @@ namespace Arnold.Api
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseStaticFiles();
 
